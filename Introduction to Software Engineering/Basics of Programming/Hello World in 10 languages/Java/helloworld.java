ac79ed8b867a51773913e8b6efa3a526422e0ba8
mmilushev91 <milushev_1990@abv.bg>
2025-08-12 16:18:51 +0300

    Create helloworld.java

---
+++ Introduction to Software Engineering/Basics of Programming/Hello World in 10 languages/Java/helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld {
+    public static void main(String s[]){
+        System.out.println("Hello World!");
+    }
+}
+
+//to compile javac helloworld.java
+//to run java helloworld
