6d9b58c086fb7a8eef40c3e64a8554319855d7b7
mmilushev91 <milushev_1990@abv.bg>
2025-08-13 10:10:25 +0300

    Create python_exercises.py

---
+++ Introduction to Software Engineering/Basics of Programming/python_exercises.py
@@ -0,0 +1,38 @@
+#1
+print("Hello World!")
+#2
+myint = 200
+print(myint)
+#3
+a = 20
+c = a * 100
+print(c)
+#4
+myval = 8
+if myval > 0:
+    print("It is a positive number")
+else: 
+    print("It is a negative number")
+#5
+myarray = [20, 10, 44, 928, 200]
+for num in myarray:
+    print(num)
+#6
+shopping_list = ["bread", "water", "chips", "candy", "soda"]
+for product in shopping_list:
+    print(product)
+
+    if product == "chips":
+        break;
+#7
+score = 60
+while score < 90:
+    print(score)
+    score += 5
+#8
+myval = 45
+while myval < 45:
+    print(myval)
+    if myval > 80:
+        break
+    myval += 8
